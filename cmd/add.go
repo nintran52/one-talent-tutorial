350ed0dc64848fc8051b0f38ee1a620053c34277
Nin Tran <nintran52@Nins-MacBook-Pro.local>
2022-07-03 23:30:11 +0700

    Add cli add int

---
+++ cmd/add.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		addInt(args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	addCmd.Flags().BoolP("float", "f", false, "Add Floating Numbers")
+}
+
+func addInt(args []string) {
+	var sum int
+
+	for _, ival := range args {
+		itemp, err := strconv.Atoi(ival)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		sum = sum + itemp
+	}
+
+	fmt.Printf("Addition of number %s is %d", args, sum)
+}
